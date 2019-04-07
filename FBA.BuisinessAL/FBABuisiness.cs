c9bda98f9b277878f2ff8d5380ac487588d450ac
vaibhavvr <vrbot.test@gmail.com>
2019-04-07 15:52:39 +0530

    Added dependent files

---
+++ FBA.BuisinessAL/FBABuisiness.cs
@@ -0,0 +1,60 @@
+﻿using FBA.DataAL;
+using FBA.DataAL.Entity;
+using System;
+using System.Collections.Generic;
+
+namespace FBA.BuisinessAL
+{
+    public class FBABuisiness
+    {
+        FBAData fbaDObj = new FBAData();
+        public List<FeedBack> GetFeedBackAnalysis(string conStr)
+        {
+             return fbaDObj.GetFeedBackAnalysis(conStr);
+        }
+        public bool AddFeedBackAnalysisCategory(string conStr,string text)
+        {
+            return fbaDObj.AddFeedBackAnalysisCategory(conStr,text);
+        }
+        public bool AddFeedBackAnalysis(string conStr, FBEntry fbDataObj)
+        {
+
+            fbDataObj.FeedBackCategoryId = AnalyseFeedBackCategory(fbDataObj.FeedBackDesc);
+            fbDataObj.FeedBackIndex = AnalyseFeedBackSentiment(fbDataObj.FeedBackDesc);
+            return fbaDObj.AddFeedBackAnalysis(conStr, fbDataObj);
+        }
+
+        private int AnalyseFeedBackCategory(string fbText)
+        {
+            int id = 0;
+
+            //TO-DO//Need to write Algo to Identify Category of feedback
+            if(fbText.Contains("payment"))
+            {
+                id = 1;
+            }
+            else if (fbText.Contains("bug"))
+            {
+                id = 2;
+            }
+            //
+           return id;
+        }
+
+        private float AnalyseFeedBackSentiment(string fbText)
+        {
+            float fbRatio = 0.0F;
+            //TO-DO//Need to call analysis Algo to Identify sentiment of feedback
+            if (fbText.Contains("good"))
+            {
+                fbRatio = 1.0F;
+            }
+            else if (fbText.Contains("bad"))
+            {
+                fbRatio = 0.1F;
+            }
+            //
+            return fbRatio;
+        }
+    }
+}
---
+++ FBA.DataAL/Entity/FBEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FBA.DataAL.Entity
+{
+    public class FBEntry
+    {
+        public int FeedBackCategoryId { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public string FeedBackDesc { get; set; }
+        public float FeedBackIndex { get; set; }
+
+    }
+}
