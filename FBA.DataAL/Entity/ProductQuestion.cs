aba346e4f6b997daacb129d12c89d0a798481672
RGaddam <rakeshkumar.gaddam@realpage.com>
2019-04-07 19:28:34 +0530

    Added product questions api

---
+++ FBA.DataAL/Entity/ProductQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FBA.DataAL.Entity
+{
+    public class ProductQuestion
+    {
+        public int questionId { get; set; }
+        public int ProductId { get; set; }
+        public string questionDescription { get; set; }
+        public string questionType { get; set; }
+    }
+}
