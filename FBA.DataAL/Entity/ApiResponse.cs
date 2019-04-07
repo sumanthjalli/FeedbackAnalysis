9c981d7b0c7198e69cc4d8bd6ed96d199f44a0ad
sumanth-jalli <sumanth.jalli@realpage.com>
2019-04-07 23:43:06 +0530

    added save feedback

---
+++ FBA.DataAL/Entity/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FBA.DataAL.Entity
+{
+   public class ApiResponse
+    {
+        public string query { get; set; }
+        public List<topScoringIntentAttributes> topScoringIntent { get; set; }
+        public List<EntityAttributes> entities { get; set; }
+        public List<LabelAttribute> sentimentAnalysis { get; set; }
+    }
+}
---
+++ FBA.DataAL/Entity/LabelAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FBA.DataAL.Entity
+{
+    public class LabelAttribute
+    {
+        public string label { get; set; }
+    }
+}
