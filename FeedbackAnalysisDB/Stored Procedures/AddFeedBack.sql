04fc177d6ca5124e7785770cb7ca302e3633092f
sumanthjalli <sumanth.jalli@gmail.com>
2019-04-08 01:36:16 +0530

    added procedures to db project

---
+++ FeedbackAnalysisDB/Stored Procedures/AddFeedBack.sql
@@ -0,0 +1,20 @@
+﻿CREATE PROC [dbo].[AddFeedBack]
+@FeedBackCategoryId int,
+@UserId int = null,
+@ProductId int,
+@FeedBackDesc varchar(MAX),
+@FeedBackIndex int
+AS
+INSERT INTO [dbo].[FeedBack]
+           ([FeedBackCategoryId]
+           ,[UserId]
+           ,[ProductId]
+           ,[FeedBackDesc]
+           ,[FeedBackIndex])
+     VALUES
+           (@FeedBackCategoryId
+           ,@UserId
+           ,@ProductId
+           ,@FeedBackDesc
+           ,@FeedBackIndex)
+GO
