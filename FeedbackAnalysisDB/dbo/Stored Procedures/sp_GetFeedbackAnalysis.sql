53178e4ee90cef285bcb81e9c11eb97572c8e820
RGaddam <rakeshkumar.gaddam@realpage.com>
2019-04-09 11:23:02 +0530

    DB Changes

---
+++ FeedbackAnalysisDB/dbo/Stored Procedures/sp_GetFeedbackAnalysis.sql
@@ -0,0 +1,13 @@
+﻿
+CREATE procedure [dbo].[sp_GetFeedbackAnalysis]
+AS
+SELECT F.[FeedBackId]
+      ,FC.[CategoryDesc]
+      ,U.[UserName]
+      ,P.[ProductName]
+      ,[FeedBackDesc]
+      ,[FeedBackIndex]
+  FROM [dbo].[FeedBack] F 
+  join [dbo].[FeedBackCategory] FC on F.[FeedBackCategoryId] = FC.[CategoryId]
+  join [dbo].[User] U on F.[UserId] = U.[UserId]
+  join [dbo].[Product] P on F.[ProductId] = P.[ProductId];
