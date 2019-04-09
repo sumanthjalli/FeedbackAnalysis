f24e0240052378a58ef5eb3cb10fd4ce11e0bca9
RGaddam <rakeshkumar.gaddam@realpage.com>
2019-04-09 11:43:00 +0530

    Revert "DB Changes"
    This reverts commit 53178e4ee90cef285bcb81e9c11eb97572c8e820.

--- FeedbackAnalysisDB/dbo/Tables/Product_Public.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Product_Public] (
-    [ProductId]   INT            IDENTITY (1, 1) NOT NULL,
-    [ProductName] NVARCHAR (MAX) NOT NULL,
-    [CompanyId]   INT            NULL,
-    PRIMARY KEY CLUSTERED ([ProductId] ASC),
-    FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Company] ([CompanyId])
-);
-
